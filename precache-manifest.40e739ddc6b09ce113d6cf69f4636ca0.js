735d5f567fc9914b67638012bbcf8c2eac996c45
Christopher Ivan <my.subscription2001@gmail.com>
2020-08-17 00:56:19 +0700

    Updates

---
+++ precache-manifest.40e739ddc6b09ce113d6cf69f4636ca0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59355de2cee8a66f2fd0f9862c9d9c9b",
+    "url": "/react-covid19-tracker/index.html"
+  },
+  {
+    "revision": "7e4e3caeac475719c205",
+    "url": "/react-covid19-tracker/static/css/main.cc5ed36b.chunk.css"
+  },
+  {
+    "revision": "e8fe90ed62512b74e6cb",
+    "url": "/react-covid19-tracker/static/js/2.7813ee7f.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/react-covid19-tracker/static/js/2.7813ee7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e4e3caeac475719c205",
+    "url": "/react-covid19-tracker/static/js/main.489a7f03.chunk.js"
+  },
+  {
+    "revision": "48aed59e2b0977aca748",
+    "url": "/react-covid19-tracker/static/js/runtime-main.b15c2239.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/react-covid19-tracker/static/media/image.d7265326.png"
+  }
+]);
